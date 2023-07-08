78f8cf682602d43b04181874cbf49452f83b1d89
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-07-09 04:51:43 +0500

    update changes

--- app.rb
+++ app.rb
@@ -1,31 +1,31 @@
 require './student'
 require './teacher'
-require './books'
-require './rent'
+require './book'
+require './rental'
 
 class App
   def initialize(parent)
     @parent = parent
     @books_list = []
     @people_list = []
-    @rents_list = []
+    @rentals_list = []
   end
 
   def list_all_books
     if @books_list.empty?
-      puts 'No Book Found!'
+      puts 'No record found! Add some books...'
     else
-      puts 'Books Available in the library'
-      @books_list.each { |books| puts "Title: #{books.title}, Author: #{books.author}" }
+      puts 'Available books in the library'
+      @books_list.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
     end
     @parent.show_menu
   end
 
   def list_all_people
     if @people_list.empty?
-      puts 'No Person Found!'
+      puts 'No record found! Add a person...'
     else
-      puts 'People in the library'
+      puts 'All people in the library'
       @people_list.each do |person|
         puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
@@ -34,7 +34,7 @@ class App
   end
 
   def create_person
-    print 'Do 'You Want to Create a Student(1) or a Teacher(2)? [Input the number]: '
+    print 'Do you want to create a student(1) or a teacher(2)? [Input the number]: '
     person_role = gets.chomp
     case person_role
     when '1'
@@ -42,7 +42,7 @@ class App
     when '2'
       create_teacher
     else
-      puts 'Please Enter a valid Number!'
+      puts 'Please add a valid input!'
     end
     @parent.show_menu
   end
@@ -52,11 +52,11 @@ class App
     age = gets.chomp.to_i
     print 'Name: '
     name = gets.chomp
-    print 'Has Parent Permission? [Y/N]: '
+    print 'Has parent permission? [Y/N]: '
     parent_permission = gets.chomp.downcase == 'y'
     @people_list.push(Student.new(age, name, parent_permission: parent_permission))
     puts
-    puts 'Student Created Successfuly'
+    puts 'Person created successfuly'
   end
 
   def create_teacher
@@ -68,7 +68,7 @@ class App
     specialization = gets.chomp
     @people_list.push(Teacher.new(age, specialization, name))
     puts
-    puts 'Teacher Created Successfuly'
+    puts 'Person created successfuly'
   end
 
   def create_book
@@ -77,13 +77,13 @@ class App
     print 'Author: '
     author = gets.chomp
     @books_list.push(Book.new(title, author))
-    puts 'Add Book successfully'
+    puts 'Book created successfully'
     @parent.show_menu
   end
 
-  def create_rent
-    puts 'Select a book from list by number'
-    @books_list.each_with_index { |books, index| puts "#{index}) Title: '#{books.title}', Author: #{books.author}" }
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books_list.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
     book_num = gets.chomp.to_i
     puts
     puts 'Select a person from the following list by number (not id)'
@@ -94,23 +94,23 @@ class App
 
     print 'Date: '
     date = gets.chomp
-    @rents_list.push(rent.new(date, @books_list[book_num], @people_list[person_num]))
-    puts 'Rent Created Successfully'
+    @rentals_list.push(Rental.new(date, @books_list[book_num], @people_list[person_num]))
+    puts 'Rental created successfully'
     @parent.show_menu
   end
 
-  def list_all_rents
+  def list_all_rentals
     print 'ID of person: '
     id = gets.chomp.to_i
 
-    puts 'rents:'
-    @rents_list.each do |rent|
-      puts "Date: #{rent.date}, Book \"#{rent.books.title}\" by #{rent.books.author}" if rent.person.id == id
+    puts 'Rentals:'
+    @rentals_list.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}" if rental.person.id == id
     end
     @parent.show_menu
   end
 
   def exit
-    puts 'Thanks for Using Library App.'
+    puts 'Thank you for using the app, see you later!'
   end
 end
